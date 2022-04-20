bf586f3607dcf689ed37fe5fecabec49c8538769
vernet55 <taison8039@yahoo.co.jp>
2022-04-20 05:00:20 +0000

    finished

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+     redirect_to book_path(@book.id), notice: "Book was successfully created."
+
+    else
+      @books = Book.all #@つけるとviewに記述しても使える＝インスタンス変数　＠をつけないということは、controllerのみでしか使用できない＝ローカル変数
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+
+    @books = Book.all
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+
+    if @book.save
+     redirect_to book_path(@book.id), notice: "Book was successfully updated."
+
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
